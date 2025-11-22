7f25caf48a4e9cad011c546714b28d37e29e3b51
Nicola Loi <nicolaloi@outlook.com>
2025-11-22 12:15:40 +0100

    Add download links for additional media files

--- download_media.sh
+++ download_media.sh
@@ -12,8 +12,13 @@ function download_with_gdown() {
     rm -f ${output_file}
 }
 
+# https://drive.google.com/file/d/1dSePXG0senGdXkzgwRf1hhvGYUF3Qdxt/view?usp=sharing
 download_with_gdown 1dSePXG0senGdXkzgwRf1hhvGYUF3Qdxt videos.zip ${current_dir}/2_Optical_Flow
+
+# https://drive.google.com/file/d/1UUMhB39Wma5sJeL-eUG7LUBtdOBpQcN4/view?usp=sharing
 download_with_gdown 1UUMhB39Wma5sJeL-eUG7LUBtdOBpQcN4 images.zip ${current_dir}/3_Visual_Odometry
+
+# https://drive.google.com/file/d/1kVYhBWxh83IqckrPOTZyJ_8zM3M0yEVz/view?usp=sharing
 download_with_gdown 1kVYhBWxh83IqckrPOTZyJ_8zM3M0yEVz image_database.zip ${current_dir}/4_Bag_of_Visual_Words
 
 echo "Downloads completed."
